b6a0d968f71fbbc79a61082282e41e1aefcb1e80
Basant-Adel <basantadel99@gmail.com>
2023-04-23 21:07:33 -0700

    I create new files _printf.c(that produces output according to a form…

--- _printf.c
+++ _printf.c
@@ -1,71 +1,51 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - A  function that produces output according to a format
  *@format: Format is a character string
  *@...: Strings to print
- *
  *Return: The number of characters printed
  *(Excluding the null byte used to end output to strings)
  */
 
 int _printf(const char *format, ...)
 {
-	int a, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int sum = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
+	char *a, *start;
+	params_t params = PARAMS_INIT;
 
 	va_start(list, format);
 
-	for (a = 0; format && format[a] != '\0'; a++)
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (a = (char *)format; *a; a++)
 	{
-		if (format[a] != '%')
+		init_params(&params, list);
+		if (*a != '%')
 		{
-			buffer[buff_ind++] = format[a];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-
-			printed_chars++;
+			sum += _putchar(*a);
+			continue;
 		}
+		start = a;
+		a++;
+		while (get_flag(a, &params))
+		{
+			a++;
+		}
+		a = get_width(a, &params, list);
+		a = get_precision(a, &params, list);
+		if (get_modifier(a, &params))
+			a++;
+		if (!get_specifier(a))
+			sum += print_from_to(start, a,
+				params.l_modifier || params.h_modifier ? a - 1 : 0);
 		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &a);
-			width = get_width(format, &a, list);
-			precision = get_precision(format, &a, list);
-			size = get_size(format, &a);
-			++a;
-			printed = handle_print(format, &a, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+			sum += get_print_func(a, list, &params);
 	}
-
-	print_buffer(buffer, &buff_ind);
-
+	_putchar(BUF_FLUSH);
 	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - It is Prints the contents of the buffer if it exist
- *@buffer: Array of char
- *@buff_ind: Add next char and Represents the length
- */
-
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	return (sum);
 }
